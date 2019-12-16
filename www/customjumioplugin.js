1c69d0a0372e3e04a19d8832ce54eba86b21d51d
Paolo Alcabao <paoloalcabao@SP-PAlcabao-MacBook-Pro-2320.local>
2019-12-16 11:26:09 +0800

    Modified to include the true Jumio plugin methods for true experimentation.

--- www/customjumioplugin.js
+++ www/customjumioplugin.js
@@ -10,6 +10,44 @@ CustomJumio.prototype.show = function(message, duration, successCallback, errorC
   cordova.exec(successCallback, errorCallback, 'CustomJumio', 'show', [options]);
 }
 
+var exec = require('cordova/exec');
+CustomJumio.prototype.initNetverify = function(token, secret, datacenter, options, customization) {
+  exec(function(success) { console.log("Netverify::init Success: " + success) }, 
+   function(error) { console.log("Netverify::init Error: " + error) },
+   "JumioMobileSDK", 
+   "initNetverify", 
+   [token, secret, datacenter, options, customization]);
+};
+
+CustomJumio.prototype.startNetverify = function(success, error) {
+  exec(success, error, "JumioMobileSDK", "startNetverify", []);
+};
+
+CustomJumio.prototype.initBAM = function(token, secret, datacenter, options, customization) {
+  exec(function(success) { console.log("BAM::init Success: " + success) }, 
+   function(error) { console.log("BAM::init Error: " + error) },
+   "JumioMobileSDK", 
+   "initBAM", 
+   [token, secret, datacenter, options, customization]);
+};
+
+CustomJumio.prototype.startBAM = function(success, error) {
+    exec(success, error, "JumioMobileSDK", "startBAM", []);
+};
+
+CustomJumio.prototype.initDocumentVerification = function(token, secret, datacenter, options, customization) {
+    exec(function(success) { console.log("DocumentVerification::init Success: " + success) }, 
+		 function(error) { console.log("DocumentVerification::init Error: " + error) },
+		 "JumioMobileSDK", 
+		 "initDocumentVerification", 
+		 [token, secret, datacenter, options, customization]);
+};
+
+CustomJumio.prototype.startDocumentVerification = function(success, error) {
+    exec(success, error, "JumioMobileSDK", "startDocumentVerification", []);
+};
+
+
 // Installation constructor that binds CustomJumio to window
 CustomJumio.install = function() {
   if (!window.plugins) {
@@ -19,3 +57,4 @@ CustomJumio.install = function() {
   return window.plugins.customJumio;
 };
 cordova.addConstructor(CustomJumio.install);
+
