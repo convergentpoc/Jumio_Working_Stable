a6d4d0a097b7d1bb8dd4f0475becff13a098a17a
convergentpoc <57383207+convergentpoc@users.noreply.github.com>
2020-02-10 17:47:07 +0800

    updated lin 25 for netverify settings
    added 
    cordova.exec(successCallback,  errorCallback, 'CustomJumio', 'initNetverify', [token,secret,datacenter,options,customization]);
    from
     cordova.exec(successCallback,  errorCallback, 'CustomJumio', 'initNetverify', [requestOptions]);

--- www/customjumioplugin.js
+++ www/customjumioplugin.js
@@ -20,8 +20,10 @@ CustomJumio.prototype.initNetverify = function(token, secret, datacenter, option
   requestOptions.datacenter = datacenter;
   requestOptions.options = options;
   requestOptions.customization = customization;
-  cordova.exec(successCallback,  errorCallback, 'CustomJumio', 'initNetverify', [requestOptions]);
-
+	//[token, secret, datacenter, options, customization]
+ // cordova.exec(successCallback,  errorCallback, 'CustomJumio', 'initNetverify', [requestOptions]);
+cordova.exec(successCallback,  errorCallback, 'CustomJumio', 'initNetverify', [token,secret,datacenter,options,customization]);
+	
   //Attempt to make it array for easier access.
   // var requestOptions = [];
   // requestOptions[0] = token;
